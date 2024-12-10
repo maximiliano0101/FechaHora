d3cee28d538077c1fef6413cf81a3ba4efa5e6c2
Maximiliano <Maximilianoaa.ti21@utsjr.edu.mx>
2024-12-10 16:18:53 -0600

    Código para agregar datos de producto

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 require("dotenv").config();
-const moment = require('moment'); // Importa moment
 const app = express();
 const ruta = require("./Rutas/ruta2");
+const logMiddleware = require("./FechaYhora/middleware"); // Importa el middleware
+
+// Aplica el middleware a todas las rutas
+app.use(logMiddleware);
 
 app.use(ruta);
 
 const port = process.env.PORT || 3000;
 
-// Muestra la fecha y hora al iniciar el servidor
+// Inicia el servidor
 app.listen(port, () => {
-    const currentTime = moment().format('YYYY-MM-DD HH:mm:ss'); // Formatea la fecha y hora
-    console.log(`Servidor en http://localhost:${port}`);
-    console.log(`Servidor iniciado el: ${currentTime}`); // Imprime la fecha y hora
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
